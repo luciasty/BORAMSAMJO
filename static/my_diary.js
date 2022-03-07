2d70c79cffd0643b4c5f583e0ced9dfa5f20df60
chocholove <gmerit0116@naver.com>
2022-03-07 14:06:26 +0900

    name change

--- static/my_diary.js
+++ static/my_diary.js
@@ -1,12 +1,40 @@
 "use strict";
 
-// // 댓글창열기/닫기
-// function myFunction() {
-//     var x = document.getElementById("comments-list");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// }
-// 댓글달기 버튼
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+        
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('.title');
+});
+
+// main
+const tabber = document.querySelector(".tabber");
+
+tabber.addEventListener("click", (e) => {
+  if (e.target.classList.contains("tab-trigger")) {
+    e.preventDefault();
+    const tab = e.target;
+    const panels = e.target.parentElement.parentElement.parentElement.querySelectorAll(
+      ".tab-panel"
+    );
+    Array.from(tab.parentElement.parentElement.children)
+      .filter((tabF) => tab !== tabF)
+      .forEach((tab) => tab.classList.remove("active"));
+    tab.parentElement.classList.add("active");
+    panels.forEach((panel) =>
+      tab.href.split("#").pop() === panel.getAttribute("content-id")
+        ? panel.classList.add("active")
+        : panel.classList.remove("active")
+    );
+  }
+});
+
---
+++ static/read_diary.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// // 댓글창열기/닫기
+// function myFunction() {
+//     var x = document.getElementById("comments-list");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+// 댓글달기 버튼
